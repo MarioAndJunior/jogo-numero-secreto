add91c035e14ce205667cc013c2046ff107537ed
MarioAndJunior <majr85@gmail.com>
2024-02-16 11:31:48 -0300

    :bug: remove '.' from guess

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -11,6 +11,7 @@ recognition.addEventListener('result', onSpeak)
 
 function onSpeak(e) {
 chute = e.results[0][0].transcript
+chute = chute.replace('.', '')
 exibeChuteNaTela(chute)
 verificaSeOChutePosuiUmValorValido(chute)
 }
